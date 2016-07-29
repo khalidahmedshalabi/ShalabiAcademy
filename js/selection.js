6dbf297d69384770ff10eb0744b7cf3e3ed091b6
Khalid <khalidahmed333@hotmail.com>
2016-07-30 01:29:25 +0200

    Start the work on the design of subject selection page

--- js/selection.js
+++ js/selection.js
@@ -1,3 +1,12 @@
 $(".gobackhomepage").click(function(event) {
     location.href = "index.html"
 });
+
+$(".gobackhomepage").slideDown(500);
+
+$(".subject_element").click(function(event) {
+    $("html, body").animate(
+	{
+		scrollTop: $(".section_show").offset().top
+	});
+});
