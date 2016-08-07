b4fb55ec88cbac750ca4d9d15a089a65da7896ef
Khalid <khalidahmed333@hotmail.com>
2016-08-07 17:37:01 +0200

    Redesigns subject selection page

--- js/selection.js
+++ js/selection.js
@@ -1,16 +1,11 @@
-$(".gobackhomepage").click(function(event) {
-    location.href = "index.html"
-});
-
-$(".gobackhomepage").slideDown(500);
 
 $(".subject_element").click(function(event) {
-    $(".subject_element").not(this).css('border-color', '#CCCCCC');
-    $(".subject_element").not(this).css('background-color', '#CCCCCC');
-    $(".subject_element").not(this).css('font-weight', 'normal');
-    $(this).css('border-color', '#666666');
-    $(this).css('background-color', '#BBBBBB');
-    $(this).css('font-weight', 'bolder');
+    $(".subject_element").not(this).not(".subject_unavailable").css('border-color', '#DDDDDD');
+    $(".subject_element").not(this).not(".subject_unavailable").css('background-color', '#DDDDDD');
+    $(".subject_element").not(this).not(".subject_unavailable").css('font-weight', 'normal');
+    $(this).not(".subject_unavailable").css('border-color', '#146942');
+    $(this).not(".subject_unavailable").css('background-color', '#CCCCCC');
+    $(this).not(".subject_unavailable").css('font-weight', 'bolder');
 
     $("html, body").animate(
 	{
@@ -18,14 +13,16 @@ $(".subject_element").click(function(event) {
 	});
 });
 
-function LoadLesson(path)
+function LoadLesson(clickedfrom, path)
 {
+    var error = 0;
     $(".video_loader").load(path + '/video.txt',
         function(response, status, xhr)
         {
             if (status == "error")
             {
                 $(".video_loader").html("<span class='loader_error'>Video is not available at the moment</span>");
+                error ++;
             }
         }
     );
@@ -36,7 +33,28 @@ function LoadLesson(path)
             if (status == "error")
             {
                 $(".paper_loader").html("<span class='loader_error'>PDF is not available at the moment</span>");
+                error ++;
             }
         }
     );
 }
+
+var MenuShown = false;
+
+$(".menu_button").click(function(event) {
+    if(MenuShown == false)
+    {
+        MenuShown = true;
+        $(".menu").css('left', '-3%');
+        $(".menu").animate({left: "3%"}, {duration: 700, queue: false});
+        $(".menu").fadeIn('500');
+    }
+});
+
+$(".close_menu_button").click(function(event) {
+    if(MenuShown == true)
+    {
+        MenuShown = false;
+        $(".menu").fadeOut('500');
+    }
+});
