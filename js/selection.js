764a4837d5261ccd66735477e51e8fe13c23e88b
Khalid <khalidahmed333@hotmail.com>
2016-08-20 21:18:04 +0200

    Adds "watch a new lesson" button at the bottom of subject selection page

--- js/selection.js
+++ js/selection.js
@@ -1,3 +1,10 @@
+$(".scrolltotop").click(function(e)
+{
+    $("html, body").animate(
+	{
+		scrollTop: 0
+	}, 1000);
+});
 
 $(".subject_element").not(".subject_sample").not(".subject_unavailable").click(function(event) {
     $(".subject_element").not(this).not(".subject_unavailable").css('border-color', 'rgba(2, 158, 2, 0.7)');
