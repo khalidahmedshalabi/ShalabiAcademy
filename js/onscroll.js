f1d206c1f138448fb85bcc5266401628f6eb528e
Khalid <khalidahmed333@hotmail.com>
2016-08-20 20:54:12 +0200

    Removes some unnecessary math

--- js/onscroll.js
+++ js/onscroll.js
@@ -1,12 +1,1 @@
 /* some fancies on scrolling */
-
-// change welcome section (top section) darkness (alpha) on scrolling
-$(window).scroll(function(event)
-{
-    var newHeight = 100 - (($(window).scrollTop() / $(window).height()) * 100);
-
-    if(newHeight > 0)
-    {
-        $(".section_welcome").css("box-shadow", "inset 0 0 0 1000px rgba(0,0,0," + (1 - (newHeight / 100)) + ")");
-    }
-});
