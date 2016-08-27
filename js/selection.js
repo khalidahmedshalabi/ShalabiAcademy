022b95aa80157e8288aa835787f65a29ada8c07d
Khalid <khalidahmed333@hotmail.com>
2016-08-27 17:42:30 +0200

    Selection page improvement and translation
    - Improves and optimizes subject selection page for smaller screens and
    touch devices.
    - Translates the page into Arabic.

--- js/selection.js
+++ js/selection.js
@@ -6,45 +6,46 @@ $(".scrolltotop").click(function(e)
 	}, 1000);
 });
 
-$(".subject_element").not(".subject_sample").not(".subject_unavailable").click(function(event) {
-    $(".subject_element").not(this).not(".subject_unavailable").css('border-color', 'rgba(2, 158, 2, 0.7)');
-    $(".subject_element").not(this).not(".subject_unavailable").css('background-color', 'rgba(2, 158, 2, 0.3)');
-    $(".subject_element").not(this).not(".subject_unavailable").css('font-weight', 'rgba(2, 158, 2, 0.9)');
-    $(this).not(".subject_unavailable").css('border-color', '#146942');
-    $(this).not(".subject_unavailable").css('background-color', '#CCCCCC');
-    $(this).not(".subject_unavailable").css('font-weight', 'bolder');
+var MenuButtonContainer = false;
 
-    $("html, body").animate(
-	{
-		scrollTop: $(".section_show").offset().top
-	});
+$(".menu_button_container").css('background-color', 'rgba(0, 0, 0, 0.08)');
+$(".menu_button_container").css('font-size', '400%');
+
+$(document).scroll(function(event) {
+    if($(document).scrollTop() > $(".menu_button_container").height())
+    {
+        if(MenuButtonContainer == false)
+        {
+            MenuButtonContainer = true;
+            $(".menu_button_container").css('display', 'none');
+            $(".menu_button_container").css('background-color', '#ffffff');
+            $(".menu_button_container").css('position', 'fixed');
+            $(".menu_button_container").css('top', '-5%');
+            $(".menu_button_container").animate({top: "0%", fontSize:"300%"}, {duration: 700, queue: false});
+            $(".menu_button_container").fadeIn('500');
+            $(".menu_button_container").css(
+                {"-webkit-box-shadow": "0px 9px 20px -10px rgba(0,0,0,0.75)"},
+                {"-moz-box-shadow": "0px 9px 20px -10px rgba(0,0,0,0.75)"},
+                {"box-shadow": "0px 9px 20px -10px rgba(0,0,0,0.75)"}
+            );
+        }
+    }
+    else
+    {
+        if(MenuButtonContainer == true)
+        {
+            MenuButtonContainer = false;
+            $(".menu_button_container").css('position', 'static');
+            $(".menu_button_container").css('background-color', 'rgba(0, 0, 0, 0.08)');
+            $(".menu_button_container").css('font-size', '400%');
+            $(".menu_button_container").css(
+                {"-webkit-box-shadow": "none"},
+                {"-moz-box-shadow": "none"},
+                {"box-shadow": "none"}
+            );
+        }
+    }
 });
-
-function LoadLesson(clickedfrom, path)
-{
-    var error = 0;
-    $(".video_loader").load(path + '/video.txt',
-        function(response, status, xhr)
-        {
-            if (status == "error")
-            {
-                $(".video_loader").html("<span class='loader_error'>Video is not available at the moment</span>");
-                error ++;
-            }
-        }
-    );
-
-    $(".paper_loader").load(path + '/paper.txt',
-        function(response, status, xhr)
-        {
-            if (status == "error")
-            {
-                $(".paper_loader").html("<span class='loader_error'>PDF is not available at the moment</span>");
-                error ++;
-            }
-        }
-    );
-}
 
 var MenuShown = false;
 
@@ -75,23 +76,3 @@ $(".close_menu_button").click(function(event) {
         $(".menu").fadeOut('500');
     }
 });
-
-function AvailableSubjectsCheckbox()
-{
-    if($(".subjects_checkbox").is(':checked'))
-    {
-        $(".subject_unavailable").not(".subject_sample").fadeOut(500);
-    }
-    else
-    {
-        $(".subject_unavailable").not(".subject_sample").fadeIn(500);
-    }
-}
-
-AvailableSubjectsCheckbox();
-
-$(".subjects_checkbox").click(function(event) {
-    AvailableSubjectsCheckbox();
-});
-
-LoadLesson(0, 'sheet/year3/hello/unit1');
---
+++ js/subjects.js
@@ -0,0 +1,59 @@
+$(".subject_element").not(".subject_sample").not(".subject_unavailable").click(function(event) {
+    $(".subject_element").not(this).not(".subject_unavailable").css('border-color', 'rgba(2, 158, 2, 0.7)');
+    $(".subject_element").not(this).not(".subject_unavailable").css('background-color', 'rgba(2, 158, 2, 0.3)');
+    $(".subject_element").not(this).not(".subject_unavailable").css('font-weight', 'rgba(2, 158, 2, 0.9)');
+    $(this).not(".subject_unavailable").css('border-color', '#146942');
+    $(this).not(".subject_unavailable").css('background-color', '#CCCCCC');
+    $(this).not(".subject_unavailable").css('font-weight', 'bolder');
+
+    $("html, body").animate(
+	{
+		scrollTop: $(".section_show").offset().top
+	});
+});
+
+function LoadLesson(clickedfrom, path)
+{
+    var error = 0;
+    $(".video_loader").load(path + '/video.txt',
+        function(response, status, xhr)
+        {
+            if (status == "error")
+            {
+                $(".video_loader").html("<span class='loader_error arkufi'>لم يتم رفع هذا الفيديو بعد</span>");
+                error ++;
+            }
+        }
+    );
+
+    $(".paper_loader").load(path + '/paper.txt',
+        function(response, status, xhr)
+        {
+            if (status == "error")
+            {
+                $(".paper_loader").html("<span class='loader_error arkufi'>هذه الاوراق غير متوفرة حالياً</span>");
+                error ++;
+            }
+        }
+    );
+}
+
+function AvailableSubjectsCheckbox()
+{
+    if($(".subjects_checkbox").is(':checked'))
+    {
+        $(".subject_unavailable").not(".subject_sample").fadeOut(500);
+    }
+    else
+    {
+        $(".subject_unavailable").not(".subject_sample").fadeIn(500);
+    }
+}
+
+AvailableSubjectsCheckbox();
+
+$(".subjects_checkbox").click(function(event) {
+    AvailableSubjectsCheckbox();
+});
+
+LoadLesson(0, 'sheet/year3/hello/unit1');
